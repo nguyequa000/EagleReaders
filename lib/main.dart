4213d41a717964a4fd3b0621ca0a4000aa050552
Zachary Riggle <106418443+ZacRiggle@users.noreply.github.com>
2026-04-18 21:05:07 -0500

    import story creation character select screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
+import 'story_character_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
