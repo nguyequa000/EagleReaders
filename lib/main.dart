09f6e3baf34decc42a6b5534a46938cece0efbbd
nguyequa000 <quannguyen4675@gmail.com>
2026-08-23 22:41:29 -0500

    Reorgainze lib, created screeen and services folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'firebase_options.dart';
-import 'login_screen.dart';
+import 'services/firebase_options.dart';
+import 'screens/login_screen.dart';
 
 
 
